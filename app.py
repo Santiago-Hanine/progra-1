93eed933969c66eb0e957907905df52b078ee8bb
Santiago Hanine <santihanine@gmail.com>
2025-04-19 11:04:51 -0300

    agrego  nuevos features

--- app.py
+++ app.py
@@ -1,23 +1,20 @@
-def comporobar_disponibilidad(opcion_sector):
+def comporobar_disponibilidad(artista, opcion_sector):
     """"Comprueba si un sector tiene cupos para la cantidad requerida"""""
     if opcion_sector == 1:
-        if capacidades[0] == 0:
-            print("AGOTADO")
-            return "AGOTADO"
+        if eventos[artista - 1][2][1] == 0:
+            return False
         else:
-            return "DISPONIBLE"
+            return True
     elif opcion_sector == 2:
-        if capacidades[1] == 0:
-            print("AGOTADO")
-            return "AGOTADO"
+        if eventos[artista - 1][3][1] == 0:
+            return False
         else:
-            return "DISPONIBLE"
+            return True
     elif opcion_sector == 3:
-        if capacidades[2] == 0:
-            print("AGOTADO")
-            return "AGOTADO"
+        if eventos[artista - 1][4][1] == 0:
+            return False
         else:
-            return "DISPONIBLE"
+            return True
 
 def seleccionar_artistas(eventos):
     """"Mestra las opciones del menu artirta y le pide al usuario que ingrese su opcion"""""
@@ -29,53 +26,44 @@ def seleccionar_artistas(eventos):
     return opcion_elegida
 
 
-def seleccionar_fecha():
-    """"Mestra las opciones del menu fecha y le pide al usuario que ingrese su opcion"""""
+def ver_fechas_del_artista(artista):
+    """"Mestra las fechas del artista elegido"""""
+    print(eventos[artista - 1])
+    fechas = []
+    for evento in eventos[artista - 1][1]:
+        fechas.append(evento)
+    return fechas
 
-    print("seleccione la fecha")
-    print("1. 22/04/25", end="")
-    print("2. 25/04/25", end="")
-    print("3. 28/04/25", end="")
 
-
-def seleccionar_sector():
+def seleccionar_sector(artista):
     """"Mestra las opciones del menu sector y le pide al usuario que ingrese su opcion"""""
     print ("Seleccione el sector:")
+    print("Sectores: ")
     print ("1. Campo")
     print("2. Platea Alta")
     print("3. Platea Baja")
     opcion_sector = int(input("Ingrese el sector: "))
-    disponibilidad = comporobar_disponibilidad(opcion_sector)
-    return disponibilidad
+    disponibilidad = comporobar_disponibilidad(artista, opcion_sector)
+    if (disponibilidad):
+        print("El sector tiene disponibilidad")
+        print("Precio: ", eventos[artista - 1][opcion_sector][1])
 
 
 
-def calcular_precio(opcion_sector):
-    """"Devuelve el precio unitario según el sector"""""
-    if opcion_sector == "1":
-        return precios["campo"]
-    elif opcion_sector == "2":
-        return precios["platea_alta"]
-    elif opcion_sector == "3":
-        return precios["platea_baja"]
-
-
-capacidades = [
-    ["campo", 1000],
-	["platea_alta", 500],
-	["platea_baja", 300],
-]
-
-precios = [
-    ["campo", 2000],
-    ["platea_alta", 3000],
-	["platea_baja", 4000]
-]
+# def calcular_precio(opcion_sector):
+#     """"Devuelve el precio unitario según el sector"""""
+#     if opcion_sector == "1":
+#         return precios["campo"]
+#     elif opcion_sector == "2":
+#         return precios["platea_alta"]
+#     elif opcion_sector == "3":
+#         return precios["platea_baja"]
 
 eventos = [
-	['Tini', '22/04/25'],
-	['Duki', '25/04/25'],
-	['Airbag', '28/04/25']
+    # Artista, Fechas donde toca     , Disponibilidad y precio de entradas (Campo )
+	['Tini', ['22/04/25', '23/04/25'], [2000, 1000], [3000, 200], [3500, 100]],
+	['Duki', ['25/04/25', '26/04/25'], [1300, 400], [2500, 400], [1500, 800]],
+	['Airbag', ['28/04/25', '29/04/25'], [2000, 200], [4000, 450], [1500, 200]]
 ]
 
 
@@ -90,8 +78,6 @@ def main():
         print("MENÚ DEL SISTEMA           ")
         print("---------------------------")
         print("[1] Ver artistas")
-        print("[2] Disponibilidad de entradas")
-        print("[3] Gestionar compra de entradas ")
         print("---------------------------")
         print("[0] Salir del programa")
         print()
@@ -99,7 +85,25 @@ def main():
         opcion = input("Seleccione una opción: ")
         if opcion in [str(i) for i in range(0, opciones)]: # Sólo continua si se elije una opcion de menú válida
             if opcion == "1":
-                print(seleccionar_artistas(eventos))
+                artista = int(seleccionar_artistas(eventos))
+                print("---------------------------")
+                print("Que desea hacer?")
+                print("---------------------------")
+                print("[1] Proceder con la compra")
+                print("[2] Ver fechas disponibles")
+                print("[3] Ver entradas disponibles")
+                opcion_elegida = int(input('Seleccione una opcion: '))
+                
+                if(opcion_elegida == 1):
+                        print(seleccionar_sector(artista))
+                elif(opcion_elegida == 2):
+                    fechas = ver_fechas_del_artista(artista)
+                    print('Fechas disponibles: ')
+                    for fecha in fechas: 
+                        print(fecha)
+                elif opcion_elegida == 3:
+                    pass
+                
         	# Opción 1
 
             elif opcion == "2":   # Opción 2
