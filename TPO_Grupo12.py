416f881931e15f1d27b49b258ab12d6140ba4985
Santiago Hanine <santihanine@gmail.com>
2025-04-19 22:04:05 -0300

    cambios finales

--- TPO_Grupo12.py
+++ TPO_Grupo12.py
@@ -18,7 +18,7 @@ def comporobar_disponibilidad(artista, opcion_sector):
 
 def seleccionar_artistas(eventos):
     """"Mestra las opciones del menu artirta y le pide al usuario que ingrese su opcion"""""
-    print("seleccione su artista")
+    print("Seleccione su artista")
     for evento in eventos:
         print("%d. %s" % (eventos.index(evento) + 1, evento[0]))
     
@@ -74,7 +74,7 @@ def seleccionar_sector(artista):
             return 4
     else:
         print("El sector no tiene disponibilidad")
-        return "No hay disponibilidad"
+        return False
 
 def modificar_precio():
     """"Modifica el precio de un sector"""""
@@ -84,6 +84,9 @@ def modificar_precio():
         return
     sector = seleccionar_sector(artista)
     nuevo_precio = int(input("Ingrese el nuevo precio: "))
+    while nuevo_precio <= 0:
+        print("El precio no puede ser negativo ni cero.")
+        nuevo_precio = int(input("Ingrese el nuevo precio: "))
     if sector == 2:
         eventos[artista - 1][2][0] = nuevo_precio
         print("Precio Campo actualizado a: ", nuevo_precio)
@@ -105,10 +108,14 @@ def modificar_disponibilidad():
     print("---------------------------")
     artista = int(seleccionar_artistas(eventos))
     sector =int(input("Ingrese el tipo de sector: campo(1), platea alta(2), platea baja(3): "))
+    while sector != 1 and sector != 2 and sector != 3:
+        print("Opción no válida. Por favor, seleccione una opción del menú.")
+        sector = int(input("Ingrese el tipo de sector: campo(1), platea alta(2), platea baja(3): "))
     nueva_disponibilidad = int(input("Ingrese la nueva disponibilidad: "))
-    print(sector, artista, nueva_disponibilidad)
+    while nueva_disponibilidad < 0:
+        print("La disponibilidad no puede ser negativa.")
+        nueva_disponibilidad = int(input("Ingrese la nueva disponibilidad: "))
     if sector == 1:
-        print(eventos[artista - 1][2][1])
         eventos[artista - 1][2][1] = nueva_disponibilidad
         print("Disponibilidad Campo actualizado a: ", nueva_disponibilidad)
     elif sector == 2:
@@ -119,16 +126,7 @@ def modificar_disponibilidad():
         print("Disponibilidad Platea Baja actualizado a: ", nueva_disponibilidad)
 
 
-eventos = [
-    # Artista, Fechas donde toca     , Precio y disponibilidad de entradas (Campo, platea alta, platea baja )
-    ['Tini', ['22/04/25', '23/04/25'], [2000, 1000], [3000, 200], [3500, 100]],
-    ['Duki', ['25/04/25', '26/04/25'], [1300, 400], [2500, 400], [1500, 800]],
-    ['Airbag', ['28/04/25', '29/04/25'], [2000, 200], [4000, 450], [1500, 200]]
-]
-
-
 #PROGRAMA PRINCIPAL
-
 def main():
     opcion = ""
     while opcion != "0":
@@ -162,13 +160,22 @@ def main():
                 print("[2] Ver fechas disponibles")
                 print("[3] Ver entradas disponibles")
                 opcion_elegida = int(input('Seleccione una opcion: '))
-                
+                while opcion_elegida != 1 and opcion_elegida != 2 and opcion_elegida != 3:
+                    print("Opción no válida. Por favor, seleccione una opción del menú.")
+                    opcion_elegida = int(input('Seleccione una opcion: '))
+            
                 if(opcion_elegida == 1):
                     sector = seleccionar_sector(artista)
+                    while sector == False:
+                        print("Sector sin entradas disponibles.")
+                        sector = seleccionar_sector(artista)
                     print("Sector elegido con exito.", )
                     cantidad_de_entradas = int(input("Ingrese la cantidad de entradas: "))
-                    while cantidad_de_entradas > eventos[artista - 1][2][1]:
+                    while cantidad_de_entradas > eventos[artista - 1][sector][1]:
                         print("No hay disponibilidad suficiente.")
+                        cantidad_de_entradas = int(input("Ingrese la cantidad de entradas: "))
+                    while cantidad_de_entradas <= 0:
+                        print("La cantidad de entradas no puede ser negativa ni cero.")
                         cantidad_de_entradas = int(input("Ingrese la cantidad de entradas: "))
                     print("Cantidad de entradas elegida: ", cantidad_de_entradas)
                     print()
@@ -215,17 +222,21 @@ def main():
                     if len(artistas_filtrados) == 0:
                         print("No hay artistas con precios menores a ", precio)
                     else:
+                        print("------------------------")
                         for artista in artistas_filtrados:
                             print("Artista: ", artista[0])
                             print("Precio: ", artista[tipo_de_sector + 1][0])
+                            print("------------------------")
                 elif opcion_de_filtro == "menor":
                     artistas_filtrados = list(filter(lambda x: x[tipo_de_sector + 1][0] < precio, eventos))
                     if len(artistas_filtrados) == 0:
                         print("No hay artistas con precios menores a ", precio)
                     else:
+                        print("------------------------")
                         for artista in artistas_filtrados:
                             print("Artista: ", artista[0])
                             print("Precio: ", artista[tipo_de_sector + 1][0])
+                            print("------------------------")
 
             elif opcion == 9:   # Opción 9
                 print()
@@ -254,7 +265,6 @@ def main():
                         if opcion_administrador == 0:
                             print("Gracias por usar el sistema de venta de entradas.")
                             print("Hasta luego!")
-                            return
                         if opcion_administrador == 1:
                             print("Modificar precios")
                             modificar_precio()
@@ -263,13 +273,37 @@ def main():
                             modificar_disponibilidad()
                         elif opcion_administrador == 3:
                             nombre = input("Ingrese el nombre del artista: ")
-                            fechas = input("Ingrese las fechas del artista (separadas por comas): ").split(",")
+                            while nombre == "":
+                                print("El nombre no puede estar vacío.")
+                                nombre = input("Ingrese el nombre del artista: ")
+                            fechas = input("Ingrese las fechas del artista (separadas por comas) dd/mm/yy: ").split(",")
+                            while len(fechas) == 0 or any(fechas) == "":
+                                print("Las fechas no pueden estar vacías.")
+                                fechas = input("Ingrese las fechas del artista (separadas por comas): ").split(",")
                             precio_campo = int(input("Ingrese el precio del campo: "))
+                            while precio_campo <= 0 or precio_campo == "":
+                                print("El precio no puede ser negativo ni cero.")
+                                precio_campo = int(input("Ingrese el precio del campo: "))
                             disponibilidad_campo = int(input("Ingrese la disponibilidad del campo: "))
+                            while disponibilidad_campo < 0 or disponibilidad_campo == "":
+                                print("La disponibilidad no puede ser negativa.")
+                                disponibilidad_campo = int(input("Ingrese la disponibilidad del campo: "))
                             precio_platea_alta = int(input("Ingrese el precio de la platea alta: "))
+                            while precio_platea_alta <= 0 or precio_platea_alta == "":
+                                print("El precio no puede ser negativo ni cero.")
+                                precio_platea_alta = int(input("Ingrese el precio de la platea alta: "))
                             disponibilidad_platea_alta = int(input("Ingrese la disponibilidad de la platea alta: "))
+                            while disponibilidad_platea_alta < 0 or disponibilidad_platea_alta == "":
+                                print("La disponibilidad no puede ser negativa.")
+                                disponibilidad_platea_alta = int(input("Ingrese la disponibilidad de la platea alta: "))
                             precio_platea_baja = int(input("Ingrese el precio de la platea baja: "))
+                            while precio_platea_baja <= 0 or precio_platea_baja == "":
+                                print("El precio no puede ser negativo ni cero.")
+                                precio_platea_baja = int(input("Ingrese el precio de la platea baja: "))
                             disponibilidad_platea_baja = int(input("Ingrese la disponibilidad de la platea baja: "))
+                            while disponibilidad_platea_baja < 0 or disponibilidad_platea_baja == "":
+                                print("La disponibilidad no puede ser negativa.")
+                                disponibilidad_platea_baja = int(input("Ingrese la disponibilidad de la platea baja: "))
                             agregar_artistas(nombre, fechas,disponibilidad_campo, precio_campo, disponibilidad_platea_alta ,precio_platea_alta, disponibilidad_platea_baja, precio_platea_baja)
                             print("Listado actualizado con exito.")
                             for evento in eventos:
@@ -281,6 +315,16 @@ def main():
                                 print("-" * 40)
                 else:
                     print("Usuario o contraseña incorrectos.")
+
+
+# Matriz de eventos
+eventos = [
+    # Artista, Fechas donde toca     , Precio y disponibilidad de entradas (Campo, platea alta, platea baja )
+    ['Tini', ['22/04/25', '23/04/25'], [2000, 1000], [3000, 200], [3500, 100]],
+    ['Duki', ['25/04/25', '26/04/25'], [1300, 400], [2500, 400], [1500, 800]],
+    ['Airbag', ['28/04/25', '29/04/25'], [2000, 200], [4000, 450], [1500, 200]]
+]
+
 
 # Punto de entrada al programa
 main()
